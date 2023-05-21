aafa146000fa4886713e78b9a45003bd7f532079
Paul Industrious <zhmerinka1pasha@gmail.com>
2023-05-21 23:33:19 +0300

    Final accord

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,5 +1,6 @@
 class Appointment < ApplicationRecord
   belongs_to :doctor, dependent: :destroy
   belongs_to :user, dependent: :destroy
+
   scope :opened, -> { where(recommendation: [nil, ""]) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
   root "doctors#index"
+
   devise_for :doctors
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
+
   resources :doctors, only: [:index, :show] do
     get 'appointments', on: :member
     patch 'update_recommendation', on: :member
   end
+
   resources :users, only: [:profile] do
     get 'appointments', to: 'users#profile', as: 'appointments', on: :member
   end
+
   resources :categories, only: [:index, :show]
   resources :appointments, only: [:new, :create]
 end
